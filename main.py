981d62b2438e23ecb7b0934669cb560e8108ac54
Nandagopal Menon <71086360+nanduuuseee@users.noreply.github.com>
2021-07-27 12:08:33 +0530

    .

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def get_prefix(bot, message):
     return commands.when_mentioned_or(*prefixes)(bot, message)
 
 
-initial_extensions = ['ranking/ranking.py']
+initial_extensions = ['ranking.ranking']
 intents = discord.Intents.default()
 intents.members = True
 bot = commands.Bot(command_prefix=get_prefix,
