0ffb5488cd4c30ee7c6e7549d931acde95988a8b
Nandagopal Menon <71086360+nanduuuseee@users.noreply.github.com>
2021-07-28 19:34:34 +0530

    Deleting heroku specific files

--- ranking/ranking.py
+++ ranking/ranking.py
@@ -1,29 +1,10 @@
 from discord.ext import commands
 import json
-from github import Github, InputGitAuthor
 
 class Cog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.file = 'ranking/rankings.json'
-
-    #     self.g = Github('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')
-    #     self.repo = self.g.get_repo('nanduuuseee/testbot-discord')
-
-    #     self.repo_file = self.repo.get_contents(self.file, ref='main')
-    #     self.data = self.repo_file.decoded_content.decode('utf-8')
-
-    # def push(self, path, message, content, branch, update=False):
-    #     author = InputGitAuthor('nandoooseee', 'nandagopalnmenon@gmail.com')
-    #     source = self.repo.get_branch('main')
-
-    #     # self.repo.create_git_ref(ref=f"refs/heads/{branch}", sha=source.commit.sha)
-    #     if update:
-    #         contents = self.repo.get_contents(path, ref=branch)
-    #         self.repo.delete_file(contents.path, 'Update Json - Delete', contents.sha)
-    #         self.repo.create_file(path, 'Update Json - Create', path)
-    #     else:
-    #         self.repo.create_file(path, message, content, branch=branch, author=author)
 
     @commands.Cog.listener()
     @commands.guild_only()
@@ -55,21 +36,9 @@ class Cog(commands.Cog):
                 }
             }
 
-        
-
         with open(self.file, 'w') as f:
             json.dump(data, f)
 
-        g = Github('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')
-
-        repo = g.get_repo('nandoooseee/testbot-discord')
-
-        content = repo.get_contents(self.file, ref='main')
-
-        repo.delete_file(self.file, 'Updating Json - 1', content.sha)
-        repo.create_file(self.file, 'Updating Json - 2', self.file)
-
-        # self.push(self.file, 'Json Updated.', self.data, 'main', update=True)
         print(data)
 
 
