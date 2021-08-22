3a7f1bb1079abb5c8414633a2fb9edd084457081
Nandagopal Menon <71086360+nandu-not-bot@users.noreply.github.com>
2021-08-22 18:10:59 +0530

    add command

--- mth/main.py
+++ mth/main.py
@@ -8,10 +8,16 @@ class Cog(commands.Cog):
     @commands.command()
     async def add(self, ctx: Context, *nums):
         try:
-            nums = list(map(int, nums))
+            nums = list(map(float, nums))
         except:
-            await ctx.send(f'Input error! use {ctx.prefix}help math {ctx.command}')
+            await ctx.send(f'Input error! use `{ctx.prefix}help math {ctx.command}`')
             return
+
+        show : str
+        for x in nums:
+            show += f'{x} + '
+
+        await ctx.send(f'{show[:-2]} = {sum(nums)}')
 
 def setup(bot):
     bot.add_cog(Cog(bot))
