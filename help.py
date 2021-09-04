71b037901ccac8eebe46298a1f498b3deff4708e
Nandagopal Menon <71086360+nandu-not-bot@users.noreply.github.com>
2021-09-04 11:20:21 +0530

    upto date with chitti

--- help.py
+++ help.py
@@ -7,51 +7,56 @@ from discord.ext.commands.context import Context
 
 class Cog(commands.Cog):
 
-    '''Help Cog for chitti'''
+    """Help Cog for chitti"""
 
     def __init__(self, bot):
         self.bot = bot
         with open("help.json", "r") as file:
             self.data = json.load(file)
 
-    @commands.command(aliases=['h'])
+    @commands.command(aliases=["h"])
     async def help(self, ctx: Context, command: str = None):
         if command is None or command not in self.data["commands"]:
             embed = discord.Embed(
-                title = 'Help command for chitti'.title(),
-                description = f'Enter `{ctx.prefix}help <command>` (without "<>") for more info on the command.',
-                color=0xB26EfA
+                title="Help command for chitti".title(),
+                description=f'Enter `{ctx.prefix}help <command>` (without "<>") for more info on the command.',
+                color=0xB26EFA,
             )
             for group in self.data["groups"]:
                 embed.add_field(
-                    name = group,
-                    value= ''.join(f"`{c}` " for c in self.data["groups"][group])
+                    name=group,
+                    value="".join(f"`{c}` " for c in self.data["groups"][group]),
+                    inline=False,
                 )
 
         else:
-            embed = discord.Embed(
-                title = f'Help: {command}',
-                colour =0xB26EfA
-            )
+            embed = discord.Embed(title=f"Help: {command}", colour=0xB26EFA)
             embed.add_field(
-                name = 'Syntax',
-                value=f'`{ctx.prefix}{command} {self.data["commands"][command]["syntax"]}`'
+                name="Syntax",
+                value=f'`{ctx.prefix}{command} {self.data["commands"][command]["syntax"]}`',
+                inline=False,
             )
             embed.add_field(
                 name="Description",
-                value=self.data['commands'][command]['help']
+                value=self.data["commands"][command]["help"],
+                inline=False,
             )
-            if self.data['commands'][command]['aliases'] != []:
+            if self.data["commands"][command]["aliases"] != []:
                 embed.add_field(
-                    name = 'Aliases',
-                    value=''.join(f'`{alias}`, ' for alias in self.data['commands'][command]['aliases'])
+                    name="Aliases",
+                    value="".join(
+                        f"`{alias}`, "
+                        for alias in self.data["commands"][command]["aliases"]
+                    ),
+                    inline=False,
                 )
 
             embed.set_footer(
-                text='DO NOT use `<>`, `[]` or `...` when using the command.\n<> = required, [] = optional, ... = infinitly continues'
+                text="DO NOT use `<>`, `[]` or `...` when using the command.\n<> = required, [] = optional, ... = infinitly continues"
             )
 
         await ctx.send(embed=embed)
+
 
 def setup(bot):
     bot.add_cog(Cog(bot))
