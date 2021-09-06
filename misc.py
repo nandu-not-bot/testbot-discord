ea1ae5710b5974b6c0a4a9aa8540090371f462e7
Nandagopal Menon <71086360+nandu-not-bot@users.noreply.github.com>
2021-09-06 21:27:40 +0530

    feedback testing

--- misc.py
+++ misc.py
@@ -10,5 +10,19 @@ class MiscCommands(commands.Cog):
         await ctx.send("This bot is cool. :)")
 
 
+    @commands.command()
+    async def feedback(self, ctx: commands.context.Context, *feedback):
+    
+        '''Get feedback from the users'''
+
+        if len(feedback) == 0:
+            ctx.send("❌ Oops! I think you forgot to give your feedback!", delete_after=5)
+            return
+    
+        dev = self.bot.get_user(591078175778537512)
+        feedback = f'{ctx.author.mention} says: '.join(f'{word} ' for word in feedback)
+
+        dev.send(feedback)
+
 def setup(bot):
     bot.add_cog(MiscCommands(bot))
