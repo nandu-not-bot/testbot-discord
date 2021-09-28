bc1dab4f34049674092a6548b0da87c3b4398eff
Nandagopal Menon <71086360+nandu-not-bot@users.noreply.github.com>
2021-09-28 14:17:17 +0530

    echo decorator

--- misc.py
+++ misc.py
@@ -5,6 +5,14 @@ class MiscCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @staticmethod
+    def get_kwargs(func: callable):
+        async def wrapper(*args):
+            string = " ".join(args)
+            await func(string)
+
+        return wrapper
+
     @commands.command(name="coolbot")
     async def coolbot(self, ctx):
         await ctx.send("This bot is cool. :)")
